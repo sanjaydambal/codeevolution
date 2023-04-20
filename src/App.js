b2e81248edda5253b940e5eebc33d11048cf2551
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-04-20 22:21:27 +0530

    PostForm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PostForm from './Components/PostForm';
+import PostalList from './Components/PostalList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <PostalList/> */}
+     <PostForm/>
     </div>
   );
 }
---
+++ src/Components/PostalList.js
@@ -0,0 +1,37 @@
+
+import axios from 'axios'
+import React, { Component } from 'react'
+
+ class PostalList extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         posts:[]
+      }
+    }
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+            console.log(response);
+            this.setState({posts:response.data})
+        })
+        .catch(error => {
+            console.log(error);
+                    })
+    }
+  render() {
+    const {posts} =this.state
+    return (
+      <div>
+        {
+            posts.length ? 
+            posts.map(post => <div key={post.id}> {post.title}</div>) :
+            null
+        }
+      </div>
+    )
+  }
+}
+
+export default PostalList
