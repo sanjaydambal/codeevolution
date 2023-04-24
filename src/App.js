7d6b2411fe4df48b60385e9e3d7e4908955ca028
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-04-24 23:42:31 +0530

    Hooks basic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import './App.css';
-import Myform from './Components/Myform';
-import PostForm from './Components/PostForm';
-import PostalList from './Components/PostalList';
+// import ClassCounter from './Components/ClassCounter';
+import HookCounter from './Components/HookCounter';
+import HookCounterTwo from './Components/HookCounterTwo';
+// import Myform from './Components/Myform';
+// import PostForm from './Components/PostForm';
+// import PostalList from './Components/PostalList';
 
 function App() {
   return (
     <div className="App">
      {/* <PostalList/> */}
      {/* <PostForm/> */}
-     <Myform/>
+     {/* <Myform/> */}
+     {/* <ClassCounter/> */}
+     {/* <HookCounter/> */}
+     <HookCounterTwo/>
     </div>
   );
 }
