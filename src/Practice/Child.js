f857b7767ed9434c6f2c2e73516b9bf3ae4bfe06
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-05-01 23:07:36 +0530

    child to parent

--- src/Practice/Child.js
+++ src/Practice/Child.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Child(props) {
-    const {name,position,college} = props
+function Child({callback}) {
+    // const {name,position,college} = props
+    const prop = 2598
+    
   return (
     <div>
-        <h1> Hello My Name is {name} and i work as {position} in {college} college.</h1>
+        {/* <h1> Hello My Name is {name} and i work as {position} in {college} college.</h1> */}
+        <button onClick={() => callback(prop)}>Submit</button>
     </div>
   )
 }
--- src/Practice/Parent.js
+++ src/Practice/Parent.js
@@ -2,10 +2,16 @@ import React from 'react'
 import Child from './Child'
 
 function Parent() {
+    const eventHandler = (e) => {
+        console.log(` Hi data passed from child is ${e} `);
+    }
   return (
     <div>
-        <Child name={"Sanjay"} position={"Asst.Professor"} college={"KLS VDIT"}/>
+        <Child callback= {eventHandler}></Child>
     </div>
+    // <div>
+    //     <Child name={"Sanjay"} position={"Asst.Professor"} college={"KLS VDIT"}/>
+    // </div>
   )
 }
 
