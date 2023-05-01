f9229bc6ea166c3be4ceac637a39802446d5e0d0
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-05-01 23:30:41 +0530

    child to child Method1

--- src/Practice/Child.js
+++ src/Practice/Child.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 function Child({callback}) {
     // const {name,position,college} = props
-    const prop = 2598
+    // const prop = 2598
     
   return (
     <div>
         {/* <h1> Hello My Name is {name} and i work as {position} in {college} college.</h1> */}
-        <button onClick={() => callback(prop)}>Submit</button>
+        {/* <button onClick={() => callback(prop)}>Submit</button> */}
+        <button onClick={() => callback(Math.floor(Math.random()*6)+1)}>Roll The Dice</button>
     </div>
   )
 }
--- src/Practice/Parent.js
+++ src/Practice/Parent.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Child from './Child'
+import ChildTwo from './ChildTwo'
 
 function Parent() {
-    const eventHandler = (e) => {
-        console.log(` Hi data passed from child is ${e} `);
-    }
+    // const eventHandler = (e) => {
+    //     console.log(` Hi data passed from child is ${e} `);
+    // }
+    const [number,setNumber] = useState(1)
   return (
     <div>
-        <Child callback= {eventHandler}></Child>
+        {/* <Child callback= {eventHandler}></Child> */}
+        <Child callback={setNumber}/>
+        <ChildTwo number={number}/>
     </div>
     // <div>
     //     <Child name={"Sanjay"} position={"Asst.Professor"} college={"KLS VDIT"}/>
